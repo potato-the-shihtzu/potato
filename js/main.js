5452f713b8642b7c1cd9ed959e57c42415c23b35
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2020-09-02 13:15:13 -0500

    added .gitignore

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// *** Jquery for turning scroll down to scroll right ***
+
+// var pos = 0;
+// $(window).bind('mousewheel DOMMouseScroll', function(event) {
+//     if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+//         pos += 50;
+//     }   else {
+//         if (pos > 1) {
+//             pos -= 50;
+//         }
+//     }
+//     $('.yscroll').scrollLeft(pos)
+// });
+
+//  *** Pure JS for moving sun and moon background ***
