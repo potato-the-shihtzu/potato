ff03048d156f113a3f98e7a41ec1f6888a95787e
Max Arnold <60246034+maxkarnold@users.noreply.github.com>
2020-09-04 21:32:53 -0500

    added hamburger menu and navigation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-// *** Jquery for turning scroll down to scroll right ***
+// Navigation Menu
+const hamburgerButton = document.querySelector("nav div svg");
+const navigation = document.querySelector("nav ul");
 
-// var pos = 0;
-// $(window).bind('mousewheel DOMMouseScroll', function(event) {
-//     if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-//         pos += 50;
-//     }   else {
-//         if (pos > 1) {
-//             pos -= 50;
-//         }
-//     }
-//     $('.yscroll').scrollLeft(pos)
-// });
-
-//  *** Pure JS for moving sun and moon background ***
+hamburgerButton.addEventListener("click", function () {
+	if (navigation.style.visibility === "hidden") {
+		navigation.style.visibility = "visible";
+	} else {
+		navigation.style.visibility = "hidden";
+	}
+});
