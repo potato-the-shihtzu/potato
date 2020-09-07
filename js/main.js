46db81d130fb5890ef20266052744347124853c0
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2020-09-07 02:09:57 -0500

    fixed navigation and added placeholder web pages

--- js/main.js
+++ js/main.js
@@ -3,9 +3,5 @@ const hamburgerButton = document.querySelector("nav div svg");
 const navigation = document.querySelector("nav ul");
 
 hamburgerButton.addEventListener("click", function () {
-	if (navigation.style.visibility === "hidden") {
-		navigation.style.visibility = "visible";
-	} else {
-		navigation.style.visibility = "hidden";
-	}
+    navigation.classList.toggle("nav_trans");
 });
