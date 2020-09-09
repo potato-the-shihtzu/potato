17e70d2db55f723bc7cd54aec4ec6da45c36e919
Max Arnold <60246034+maxkarnold@users.noreply.github.com>
2020-09-09 14:28:13 -0500

    made fixes to schedule

--- js/main.js
+++ js/main.js
@@ -9,22 +9,44 @@ hamburgerButton.addEventListener("click", function () {
 // *** Dog Log ***
 
 // Selectors
-const logDate = document.querySelector(".log_date");
-const logTime = document.querySelector(".log_time");
-const logActivity = document.querySelector(".log_activity");
-const logComment = document.querySelector(".log_comment");
 
-const logEntry = [logDate, logTime, logActivity, logComment];
+const logDate = document.querySelector("form .log_date");
+const logTime = document.querySelector("form .log_time");
+const logActivity = document.querySelector("form .log_activity");
+const logComment = document.querySelector("form .log_comment");
+
+const logParamaters = [logDate, logTime, logActivity, logComment];
 
 const logButton = document.querySelector(".log_button");
 const logContainer = document.querySelector(".log_container");
 const logList = document.querySelector(".log_list");
 
+const filterOption = document.querySelector(".filter_log");
+
 // Event Listeners
-logButton.addEventListener("click", addLogEntry);
+window.addEventListener("load", defaultDateTime);
+logButton.addEventListener(
+	"click",
+	addLogEntry(e, formattedDate, formattedTime)
+);
 logList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterLog);
+
 // Functions
-function addLogEntry(event) {
+
+function defaultDateTime() {
+	// setting Default Time and Date
+	let currentTimeDate = newDate();
+
+	if (currentTimeDate.getMonth() < 9) {
+		let month = 0(currentTimeDate.getMonth() + 1);
+	}
+	// setting formatted Time and Date
+	let formattedDate = "";
+	return formattedDate, formattedTime;
+}
+
+function addLogEntry(event, formattedDate, formattedTime) {
 	// Prevent form from submitting
 	event.preventDefault();
 	// Create logEntry div
@@ -32,12 +54,28 @@ function addLogEntry(event) {
 	logDiv.classList.add("log_entry");
 
 	// Create each element of logItem
-	for (var i = 0; i < logEntry.length; i++) {
-		if (logEntry[i].value !== "") {
-			const newLogItem = document.createElement("li");
-			console.log((newLogItem.innerHTML = logEntry[i].value.replace("_", " ")));
-			console.log(newLogItem.classList.add("log_item"));
-			console.log(logDiv.appendChild(newLogItem));
+
+	for (let i = 0; i < logParamaters.length; i++) {
+		const newLogItem = document.createElement("li");
+		if (logParamaters[i].value !== "") {
+			// For date
+			if (logParamaters[i] === logDate) {
+				newLogItem.innerHTML = formattedDate;
+			}
+			// for time
+			else if (logParamaters[i] === logTime) {
+				newLogItem.innerHTML = formattedTime;
+			}
+			// for activity
+			else if (logParamaters[i] === logActivity) {
+				newLogItem.innerHTML = logActivity.value.replace(/_/g, " ");
+			}
+			// for optional comment
+			else if (logParamaters[i] === logComment) {
+				newLogItem.innerHTML = logComment.value;
+			}
+			newLogItem.classList.add(logParamaters[i].className);
+			logDiv.appendChild(newLogIem);
 		}
 	}
 
@@ -50,20 +88,50 @@ function addLogEntry(event) {
 	logList.appendChild(logDiv);
 
 	// Clear log input value
-	for (var i = 0; i < logEntry.length; i++) {
-		logEntry[i].value = "";
-	}
+	// for (let i = 0; i < logParamaters.length; i++) {
+	// 	logParamaters[i].value = "";
+	// }
 }
 
-function deleteCheck(event){
+function deleteCheck(event) {
 	const logItem = event.target;
 	// Delete logEntry
-	if (logItem.classList[0] === "trash_btn"){
+	if (logItem.classList[0] === "trash_btn") {
 		const parentLogItem = logItem.parentElement;
 		// Animation
 		parentLogItem.classList.add("fall");
-		window.setTimeout(function(){parentLogItem.remove()}, 1000);
-		// parentLogItem.addEventListener('transitionend', function(){parentLogItem.remove()});
+		window.setTimeout(function () {
+			parentLogItem.remove();
+		}, 1000);
 	}
+}
 
+function filterLog(e) {
+	// child elements of logList
+	const logEntries = logList.childNodes;
+	// for all children of logList
+	logEntries.forEach(function (logEntry) {
+		// child elements of individual logEntry
+		const logListItems = logEntry.childNodes;
+		console.log(logEntry);
+		console.log(logListItems);
+		console.log(e.target.value);
+		// show list items based on filter option
+		switch (e.target.value) {
+			case "all":
+				logEntry.style.display = "flex";
+				break;
+			case "with_comment":
+				for (const logLi of logListItems) {
+					console.log(logLi);
+					if (logLi.classList.contains("log_comment")) {
+						logEntry.style.display = "flex";
+						break;
+					} else if (logLi.classList.contains("trash_btn")) {
+						logEntry.style.display = "none";
+					}
+				}
+				break;
+		}
+	});
 }
